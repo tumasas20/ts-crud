063de7cbfb6dfd895a8ba37d122df33c46ac50f1
tumasas20 <tumasas20@gmail.com>
2023-01-24 22:35:34 +0200

    feat: added types

--- src/types/car-joined.ts
+++ src/types/car-joined.ts
@@ -1,5 +1,10 @@
-type CarJoined = {
+import Car from "./car";
+import Model from "./model";
+import Brand from "./brand";
 
+type CarJoined = Omit<Car, 'modelId'> &{
+    brand: Brand['title'],
+    model: Model['title'],
 };
 
 export default CarJoined;
--- src/types/car.ts
+++ src/types/car.ts
@@ -1,5 +1,8 @@
 type Car = {
-
+    id: number,
+    price: number,
+    year: number,
+    modelId: string,
 };
 
 export default Car;
