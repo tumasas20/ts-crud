f4b4bcef2cd6234b1fec70faa4f9f13b4597b5f8
tumasas20 <tumasas20@gmail.com>
2023-01-25 11:49:43 +0200

    feat: init app component

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,8 +1,16 @@
+import cars from '../data/cars';
+import brands from '../data/brands';
+import models from '../data/models';
+import CarsCollection from '../helpers/cars-collection';
+
 class App {
   private htmlElement: HTMLElement;
 
+  private carsCollection: CarsCollection;
+
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
+    this.carsCollection = new CarsCollection({ cars, brands, models });
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
